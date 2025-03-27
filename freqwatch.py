fb2a600b20ff5fc90f983a0f4fcabcd20c97755c
DetailTech <92436065+DetailTech@users.noreply.github.com>
2025-03-27 09:42:36 -0400

    Update freqwatch.py
    v1 update to python3

--- freqwatch.py
+++ freqwatch.py
@@ -1,4 +1,7 @@
-#!/usr/bin/env python2
+#DetailTech fork of freqwatch, updating to python3
+
+###original comments###
+#!/usr/bin/env python3
 # freqwatch v0.1
 #
 # Joshua Davis (freqwatch -*- covert.codes)
@@ -21,7 +24,7 @@
 from __future__ import division
 
 import cgi
-import MySQLdb
+import pymysql
 import math
 import os
 import signal
@@ -36,8 +39,8 @@ stop = threading.Event()
 
 BLACKLIST_FILE = 'blacklist'
 CONF_FILE = 'freqwatch.conf'
-ERR              = 1
-FW_VERSION       = 'v0.1'
+ERR = 1
+FW_VERSION = 'v0.1'
 
 
 class Param:
@@ -51,17 +54,16 @@ class Param:
                     return p[1].strip()
                 except:
                     return None
-
         return None
 
 
-class Scanner():
+class Scanner:
     def __init__(self, devid, freqs, squelch, ppm, params):
         self.devid = devid
         self.freqs = freqs
         self.squelch = squelch
         self.ppm = ppm
-        self.cmd = params.getparam('rtl_path')+'/rtl_power'
+        self.cmd = params.getparam('rtl_path') + '/rtl_power'
         self.delay = float(params.getparam('collection_delay'))
         self.params = params
         self.db_scan_table = params.getparam('db_scan_table')
@@ -69,19 +71,19 @@ class Scanner():
         self.devnull = open(os.devnull, 'w')
 
         try:
-            self.db = MySQLdb.connect(host=params.getparam('db_ip'), \
-                                      port=int(params.getparam('db_port')), \
-                                      user=params.getparam('db_user'), \
-                                      passwd=params.getparam('db_pass'), \
+            self.db = pymysql.connect(host=params.getparam('db_ip'),
+                                      port=int(params.getparam('db_port')),
+                                      user=params.getparam('db_user'),
+                                      password=params.getparam('db_pass'),
                                       db=params.getparam('db_db'))
 
             self.db.autocommit(False)
             self.cursor = self.db.cursor()
 
-        except MySQLdb.Error, e:
+        except pymysql.Error as e:
             print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
 
-        self.sql = ('INSERT INTO {} (date, time, freq, power) VALUES ''(%s, %s, %s, %s)'.format(self.db_scan_table))
+        self.sql = ('INSERT INTO {} (date, time, freq, power) VALUES (%s, %s, %s, %s)'.format(self.db_scan_table))
 
         # blacklist
         self.blacklist = list()
@@ -89,90 +91,74 @@ class Scanner():
         try:
             with open(BLACKLIST_FILE) as bfile:
                 blines = bfile.readlines()
-
         except:
             print("Could not import from blacklist file {}".format(BLACKLIST_FILE))
 
-        if blines != None:
+        if blines is not None:
             for b in blines:
                 if b[0] == '#' or '-' not in b:
                     continue
-
                 try:
                     f1, f2 = b.split('-')
                     self.blacklist.append([int(f1), int(f2)])
                 except:
                     pass
 
-
     def worker(self):
         print("Scanner thread running for device {}".format(self.devid))
 
         while not stop.isSet():
-            p = Popen([self.cmd, "-d {}".format(self.devid), "-f {}".format(self.freqs), \
-                    "-1", "-p {}".format(self.ppm)], stdout=PIPE, stderr=self.devnull)
+            p = Popen([self.cmd, "-d {}".format(self.devid), "-f {}".format(self.freqs),
+                       "-1", "-p {}".format(self.ppm)], stdout=PIPE, stderr=self.devnull, universal_newlines=True)
 
             data = p.communicate()[0].strip()
             rc = p.returncode
 
             if rc != 0:
-                print("rtl-power exited with error code that was not 0({}); "\
-                        "thread terminating".format(rc))
+                print("rtl-power exited with error code that was not 0({}); thread terminating".format(rc))
 
             for tmp in data.split('\n'):
                 if stop.isSet():
                     return
-
                 try:
-                    d, t, freq_low, freq_high, freq_step, samples, \
-                            raw_readings = tmp.split(', ', 6)
+                    d, t, freq_low, freq_high, freq_step, samples, raw_readings = tmp.split(', ', 6)
                     freq_low = float(freq_low)
                     freq_step = float(freq_step)
-
                     readings = [x.strip() for x in raw_readings.split(',')]
                 except:
                     print("exiting either because sticks not connected, or ctrl-c")
                     sys.exit(0)
 
                 for i in range(len(readings)):
-                    f = freq_low+(freq_step*i)
-
-                    if self.blacklisted(f) == True:
+                    f = freq_low + (freq_step * i)
+                    if self.blacklisted(f):
                         continue
-
                     r = float(readings[i])
-
                     if r > self.squelch:
                         self.insertdb(d, t, f, r)
 
             self.db.commit()
             time.sleep(self.delay)
 
-        return
-
-
     def blacklisted(self, freq):
         for b in self.blacklist:
             if freq > b[0] and freq < b[1]:
                 return True
-
         return False
-
 
     def insertdb(self, d, t, freq, power):
         try:
             self.cursor.execute(self.sql, (d, t, freq, power))
-
-        except MySQLdb.Error, e:
+        except pymysql.Error as e:
             self.db.rollback()
             print("MySQL Error [{}]: {}".format(e.args[0], e.args[1]))
             sys.exit(ERR)
 
 
-class Collector():
+class Collector:
     def __init__(self, params):
         scanners = params.getparam('scanners')
-        if scanners == None:
+        if scanners is None:
             print("No scanners defined in the configuration file.")
             sys.exit(ERR)
 
@@ -183,16 +169,14 @@ class Collector():
             devid = s.strip()
             if len(devid) == 0:
                 continue
-
             if int(devid) in devs:
                 print("You already have assigned the device id {}".format(devid))
                 sys.exit(ERR)
             devs.append(int(devid))
 
-            freqs, squelch, ppm = params.getparam('scanner'+s).split('/')
-            if freqs == None:
-                print("Is there a frequency entry for each device you've " \
-                        "specified in 'scanners' in the configuration?")
+            freqs, squelch, ppm = params.getparam('scanner' + s).split('/')
+            if freqs is None:
+                print("Is there a frequency entry for each device you've specified in 'scanners' in the configuration?")
                 sys.exit(ERR)
 
             freqs = freqs.strip()
@@ -221,16 +205,13 @@ def main():
         sys.exit(ERR)
 
     param_list = list()
-
     for l in lines:
         l = l.strip()
         if len(l) == 0 or l[0] == '#':
             continue
-
         p = l.split('=')
         if len(p) != 2:
             continue
-
         param_list.append(p)
 
     params = Param(param_list)
@@ -239,15 +220,12 @@ def main():
 
 def sigint_handler(signal, frame):
     global sigint_handled
-
     if sigint_handled == 0:
-        print("SIGINT received.  Stopping...")
+        print("SIGINT received. Stopping...")
         stop.set()
         sigint_handled = 1
 
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, sigint_handler)
-
     main()
-
